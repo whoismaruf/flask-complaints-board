43967b56e74223a64beec8ccabb57eb36c1d6bd1
whoismaruf <whoismaruf@outlook.com>
2021-08-01 00:27:24 +0600

    initialized flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__,
+            template_folder='templates',
+            static_folder='static'
+            )
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
